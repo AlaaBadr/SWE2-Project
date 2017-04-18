f78f917718bbd1be702b4abdf9f6866df775c7a6
Alaa Atef Badr <alaa.badr.25@gmail.com>
2017-04-19 00:13:12 +0200

    CrossOrigin

--- Edugame/src/main/java/com/edugame/course/CourseController.java
+++ Edugame/src/main/java/com/edugame/course/CourseController.java
@@ -3,6 +3,7 @@ package com.edugame.course;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,36 +18,42 @@ public class CourseController {
 	@Autowired
 	private CourseService courseService;
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/courses")
 	public List<Course> showCourses()
 	{
 		return courseService.getAllCourses();
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/Courses/{teacherUsername}")
 	public List<Course> showTeacherCourses(@PathVariable("teacherUsername") String username)
 	{
 		return courseService.getAllCoursesByTeacher(username);
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/enrolledCourses/{studentUsername}")
 	public List<Course> showStudentCourses(@PathVariable("studentUsername") String username)
 	{
 		return courseService.getAllCoursesByStudent(username);
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/courses/{courseName}")
 	public Course getCourse(@PathVariable("courseName") String name)
 	{
 		return courseService.getCourse(name);
 	}
 	
+	@CrossOrigin()
 	@PostMapping("/edugame/courses/{teacherUsername}/addCourse")
 	public Course saveCourse(@PathVariable("teacherUsername") String username, @RequestBody Course c) throws InterruptedException
 	{
 		return courseService.saveCourse(c, username);
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/courses/{courseName}/enroll/{studentUsername}")
 	public Boolean enrollStudent(@PathVariable("courseName") String courseName, @PathVariable("studentUsername") String username)
 	{
--- Edugame/src/main/java/com/edugame/level/LevelController.java
+++ Edugame/src/main/java/com/edugame/level/LevelController.java
@@ -3,6 +3,7 @@ package com.edugame.level;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +14,21 @@ public class LevelController {
 	@Autowired
 	private LevelService levelService;
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/courses/games/{gameName}/levels/{levelno}")
 	public Level getLevel(@PathVariable("gameName") String gameName, @PathVariable("levelno") int levelno)
 	{
 		return levelService.getSpecificLevel(gameName, levelno);
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/courses/games/{gameName}/levels")
 	public List<Level> getLevels(@PathVariable("gameName") String gameName)
 	{
 		return levelService.getLevels(gameName);
 	}
 	
+	@CrossOrigin()
 	@GetMapping("/edugame/checkAnswer/{username}/{gameName}/{levelno}/{rightAnswer}")
 	public Boolean addAchievement(@PathVariable("username") String username,
 								  @PathVariable("gameName") String gameName,
